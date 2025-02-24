f964261b234694f419c1fb6d67b386367c00cfb9
Paula <paulare17@gmail.com>
2025-02-24 12:19:49 +0100

    Exercici 1 i 2 finalitzat

--- src/films.js
+++ src/films.js
@@ -1,13 +1,15 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
+  let result =  array.map(movie => movie.director);
   console.log("EXERCICE 1 ->", result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+ let searchForDirector = array.filter(movie => movie.director === director);
+ console.log("EXERCICE 2 ->", searchForDirector)
+ return searchForDirector;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
